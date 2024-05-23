479239bd57623996b7d5f486d410638d2e1d98ea
JeanRosselVallee <jv.isa4031@gmail.com>
2024-05-23 21:45:18 +0000

    Added Port Choice Staging/Prouction to Website Form

--- website/web_router.py
+++ website/web_router.py
@@ -15,12 +15,13 @@ def form():
 
 @app.route('/result/', methods=['POST'])                  # result predicted by API
 def result():    
-    dict_features = request.form.to_dict(flat=False) #{umap_x:[.3],umap_y:[.3]}
-    req_post = requests.post(   url     = 'http://localhost:5678/invocations', 
+    dict_features = request.form.to_dict(flat=False) #{port:[5678],umap_x:[.3],umap_y:[.3]}
+    port = dict_features.pop('port')[0]
+    req_post = requests.post(   url     = 'http://localhost:' + str(port) + '/invocations', 
                                 headers = {'Content-Type': 'application/json'}, 
                                 data    = json.dumps({'inputs': dict_features}) )
     dict_prediction = json.loads(req_post.text)     #{predictions:[1]}
-    return render_template('result.html', features=dict_features, target_value=dict_prediction)
+    return render_template('result.html', port=port, features=dict_features, target_value=dict_prediction)
 
 @app.route('/deploy/')                                    # deploy
 def deploy():
