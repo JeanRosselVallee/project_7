70b02f30c157bd565892fce5d5641d36f13034cb
JeanRosselVallee <jv.isa4031@gmail.com>
2024-05-23 22:45:22 +0000

    Tests executed only on model at Staging server

--- test_api/test_1.py
+++ test_api/test_1.py
@@ -1,7 +1,7 @@
 import requests
 def test_connection():    
     print('__________Connection to API Server_____________')
-    req_post = requests.post(   url     = 'http://13.92.86.145:5678/invocations', 
+    req_post = requests.post(   url     = 'http://13.92.86.145:5677/invocations', 
                                 headers = {'Content-Type': 'application/json'}, 
                                 data    = '{"inputs": {"umap_x": [".3"], "umap_y": [".3"]}}' )
     is_connected = req_post.ok
