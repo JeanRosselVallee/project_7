f3716861f699cea90b063171b7ae87649fec5c42
JeanRosselVallee <jv.isa4031@gmail.com>
2024-05-24 09:35:22 +0000

    Added check process after Model Server restarts

--- website/web_router.py
+++ website/web_router.py
@@ -26,17 +26,25 @@ def result():
 
 @app.route('/deploy_to_staging/')                         # deploy to Staging
 def deploy():
-    pull()                                                # phase_1 git pull model
-    restart()                                             # phase_2 restart model server
+    str_output  = 'Git Pull Model:\n' + pull()             # phase_1 git pull model
+    str_output += 'Restart Server:\n' + restart()          # phase_2 restart model server
+    return str_output
 def pull():
     dir_root = '/home/azureuser/project_7/'
-    shell_command = 'cd ' + dir_root + ' ; git pull origin main'
-    shell_process = subprocess.run([shell_command], shell=True, capture_output=True, text=True)
-    return shell_process.stdout + shell_process.stderr
+    str_command_pull = 'cd ' + dir_root + ' ; git pull origin main'
+    str_output = run_shell(str_command_pull)
+    return str_output
 def restart():
     port_staging_server = '5677'
-    shell_command = 'pkill -f ":' + port_staging_server + '" ;' + \
-        'mlflow models serve -m ../api/staging_model/ -p ' + port_staging_server + ' -h 0.0.0.0 --no-conda & ;' + \
-        'ps aux | grep  ":' + port_staging_server + '"'
-    shell_process = subprocess.run(shell_command) 
-    return shell_process.stdout + shell_process.stderr
+    str_command_serve = 'mlflow models serve -m ../api/staging_model/ -p ' + port_staging_server + ' -h 0.0.0.0 --no-conda &'
+    str_command_ps = 'ps aux | grep  ":' + port_staging_server + '" | grep -v grep | awk \'{print $2, $15, $19}\' '
+    str_output = 'Process BEFORE Restart:\n'             # check process BEFORE restart
+    str_output += run_shell(str_command_ps)
+    run_shell('pkill -f ":' + port_staging_server + '" ; sleep 1')     # RESTART
+    subprocess.Popen(str_command_serve, start_new_session=True, shell=True)
+    str_output += 'Process AFTER Restart:\n'           # check process AFTER restart
+    str_output += run_shell('sleep 4 ; ' + str_command_ps)
+    return str_output
+def run_shell(command):
+    shell_process = subprocess.run([command], shell=True, capture_output=True, text=True)
+    return str(shell_process.stdout) + str(shell_process.stderr)
