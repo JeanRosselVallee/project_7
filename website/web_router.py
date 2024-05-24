9cc9e7d4edfdb7683ef9a662c31aa05d56ba3ee0
JeanRosselVallee <jv.isa4031@gmail.com>
2024-05-24 11:58:38 +0000

    Added Deploy to Production in web_router.py

--- website/web_router.py
+++ website/web_router.py
@@ -1,20 +1,41 @@
-
 from flask import Flask, render_template, request
 import requests
 import json
 import subprocess
+ 
+def run_shell(command):
+    shell_process = subprocess.run([command], shell=True, capture_output=True, text=True)
+    return str(shell_process.stdout) + str(shell_process.stderr)
+def pull():
+    dir_root = '/home/azureuser/project_7/'
+    str_command_pull = 'cd ' + dir_root + ' ; git pull origin main'
+    str_output = run_shell(str_command_pull)
+    return 'Git Pull Model:===============\n' + str_output
+def restart(port_staging_server):
+    str_command_serve = 'mlflow models serve -m ../api/staging_model/ -p ' + port_staging_server + ' -h 0.0.0.0 --no-conda &'
+    str_command_ps = 'ps aux | grep  ":' + port_staging_server + '" | grep -v grep | awk \'{print $2, $15, $19}\' '
+    str_output = 'Process BEFORE Restart:\n'                    # check process BEFORE restart
+    str_output += run_shell(str_command_ps)
+    run_shell('pkill -f ":' + port_staging_server + '" ; sleep 1')     # RESTART
+    subprocess.Popen(str_command_serve, start_new_session=True, shell=True)
+    str_output += 'Process AFTER Restart:\n'                    # check process AFTER restart
+    str_output += run_shell('sleep 4 ; ' + str_command_ps)
+    return 'Restart Server:===============\n' + str_output
+def copy_model():
+    str_command_cp = 'cp -rf ../api/staging_model/* ../api/production_model/ ; echo $?'
+    str_output = run_shell(str_command_cp)
+    return str_output
 
-app = Flask(__name__) # get work dirs
+# WebApp Router
+app = Flask(__name__)                                     
 
-@app.route('/')                                           # index
-def index():
-    return render_template('index.html') # HTML as string
+@app.route('/')                                           # Route Index
+def index(): return render_template('index.html')         # renders HTML as string
 
-@app.route('/form/')                                      # form
-def form():
-    return render_template('form.html')
+@app.route('/form/')                                      # Route Form
+def form(): return render_template('form.html')
 
-@app.route('/result/', methods=['POST'])                  # result predicted by API
+@app.route('/result/', methods=['POST'])                  # Route Result (API prediction)
 def result():    
     dict_features = request.form.to_dict(flat=False) #{port:[5678],umap_x:[.3],umap_y:[.3]}
     port = dict_features.pop('port')[0]
@@ -24,27 +45,19 @@ def result():
     dict_prediction = json.loads(req_post.text)     #{predictions:[1]}
     return render_template('result.html', port=port, features=dict_features, target_value=dict_prediction)
 
-@app.route('/deploy_to_staging/')                         # deploy to Staging
-def deploy():
-    str_output  = 'Git Pull Model:\n' + pull()             # phase_1 git pull model
-    str_output += 'Restart Server:\n' + restart()          # phase_2 restart model server
+@app.route('/deploy_to_staging/')                         # Deploys to Staging
+def deploy_staging():
+    str_output  = pull()                                        # phase_1 git pull model
+    str_output += restart('5677')                               # phase_2 restart model server
     return str_output
-def pull():
-    dir_root = '/home/azureuser/project_7/'
-    str_command_pull = 'cd ' + dir_root + ' ; git pull origin main'
-    str_output = run_shell(str_command_pull)
+
+@app.route('/deploy_to_production/')                      # Deploys to Production
+def deploy_production():
+    str_output  = 'Copy Model from Staging to Production:===================\n' 
+    str_return_code = copy_model()                              # phase_1 copy model
+    if str_return_code == '0\n' :
+        str_output += 'OK: Model copied'
+        str_output += restart('5678')                           # phase_2 restart model server
+    else : 
+        str_output += 'ERROR: Model could NOT be copied'
     return str_output
-def restart():
-    port_staging_server = '5677'
-    str_command_serve = 'mlflow models serve -m ../api/staging_model/ -p ' + port_staging_server + ' -h 0.0.0.0 --no-conda &'
-    str_command_ps = 'ps aux | grep  ":' + port_staging_server + '" | grep -v grep | awk \'{print $2, $15, $19}\' '
-    str_output = 'Process BEFORE Restart:\n'             # check process BEFORE restart
-    str_output += run_shell(str_command_ps)
-    run_shell('pkill -f ":' + port_staging_server + '" ; sleep 1')     # RESTART
-    subprocess.Popen(str_command_serve, start_new_session=True, shell=True)
-    str_output += 'Process AFTER Restart:\n'           # check process AFTER restart
-    str_output += run_shell('sleep 4 ; ' + str_command_ps)
-    return str_output
-def run_shell(command):
-    shell_process = subprocess.run([command], shell=True, capture_output=True, text=True)
-    return str(shell_process.stdout) + str(shell_process.stderr)
