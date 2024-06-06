7bef40cecdc6714e8f399c31155e3a7e02365c77
JeanRosselVallee <jv.isa4031@gmail.com>
2024-06-06 18:21:52 +0000

    Total re-installation of packages on new VM after Azure subscription end

--- test_api/test_1.py
+++ test_api/test_1.py
@@ -1,7 +1,7 @@
 import requests
 def test_connection():    
     print('__________Connection to API Server_____________')
-    req_post = requests.post(   url     = 'http://13.92.86.145:5677/invocations', 
+    req_post = requests.post(   url     = 'http://4.233.201.217/:5677/invocations', 
                                 headers = {'Content-Type': 'application/json'}, 
                                 data    = '{"inputs": {"CODE_GENDER_M":1,"NAME_CONTRACT_TYPE_Cash_loans":1,"NAME_EDUCATION_TYPE_Lower_secondary":0,"EXT_SOURCE_3":1.0,"NAME_EDUCATION_TYPE_Higher_education":1,"NAME_EDUCATION_TYPE_Secondary_or_secondary_special":0}}' )  
     is_connected = req_post.ok
