73252e380d09c3eb3e74cf3b890b32ca6e4bf049
JeanRosselVallee <jv.isa4031@gmail.com>
2024-06-25 16:20:46 +0200

    Updated Test & Web Pages with new top features

--- test_api/test_1.py
+++ test_api/test_1.py
@@ -3,8 +3,9 @@ def test_connection():
     print('__________Connection to API Server_____________')
     req_post = requests.post(   url     = 'http://4.233.201.217:5677/invocations', 
                                 headers = {'Content-Type': 'application/json'}, 
-                                data    = '{"inputs": {"CODE_GENDER_M":1,"NAME_CONTRACT_TYPE_Cash_loans":1,"NAME_EDUCATION_TYPE_Lower_secondary":0,"EXT_SOURCE_3":1.0,"NAME_EDUCATION_TYPE_Higher_education":1,"NAME_EDUCATION_TYPE_Secondary_or_secondary_special":0}}' )  
+                                data    = '{"dataframe_split": {"index": [0], "columns": ["NAME_EDUCATION_TYPE_Higher_education", "EXT_SOURCE_3", "EXT_SOURCE_2", "CODE_GENDER_M", "NAME_EDUCATION_TYPE_Secondary_or_secondary_special", "NAME_CONTRACT_TYPE_Cash_loans", "NAME_INCOME_TYPE_Working"], "data": [[0, 0.5638350489514956, 0.3058183171273599, 0, 1, 1, 1]]} }' )
+    # Evaluation of response
     is_connected = req_post.ok
-    if is_connected : print('OK : Connected to API Server')
+    if is_connected : print('OK : Connected to API Server & validated input format')
     assert is_connected
 
--- test_api/test_2.py
+++ test_api/test_2.py
@@ -1,26 +1,22 @@
+
 import requests
+import pandas as pd
 import json
 
-def predict(dict_features):
-    # Send input data to prediction API
-    req_post = requests.post(   url     = 'http://4.233.201.217:5677/invocations',
-                                headers = {'Content-Type': 'application/json'},
-                                data    = json.dumps({'inputs': dict_features}) )
-    # Get predicted value from API
-    dict_predicted = json.loads(req_post.text)
-    return dict_predicted
+def test_predict():
+    dict_inputs = {"index": [2582, 2583, 2584], "columns": ["NAME_EDUCATION_TYPE_Higher_education", "EXT_SOURCE_3", "EXT_SOURCE_2", "CODE_GENDER_M", "NAME_EDUCATION_TYPE_Secondary_or_secondary_special", "NAME_CONTRACT_TYPE_Cash_loans", "NAME_INCOME_TYPE_Working"], "data": [[0, 0.3944954053123993, 0.2731892846252296, 0, 1, 1, 1], [0, 0.1566398270314114, 0.5342365946929042, 1, 1, 1, 0], [0, 0.1176137317080569, 0.4562911010223129, 0, 1, 1, 0]]}                     # input observations      
+    df_result_grid = pd.DataFrame(data=dict_inputs['data'], columns=dict_inputs['columns'])
+    df_result_grid['target'] = [1, 1, 1]      # expected output
 
-def test_prediction():
-    li_dict_features = [{"CODE_GENDER_M":0,"NAME_CONTRACT_TYPE_Cash_loans":1,"NAME_EDUCATION_TYPE_Lower_secondary":1,"EXT_SOURCE_3":0.1359510442,"NAME_EDUCATION_TYPE_Higher_education":0,"NAME_EDUCATION_TYPE_Secondary_or_secondary_special":0},{"CODE_GENDER_M":0,"NAME_CONTRACT_TYPE_Cash_loans":1,"NAME_EDUCATION_TYPE_Lower_secondary":1,"EXT_SOURCE_3":0.244516392,"NAME_EDUCATION_TYPE_Higher_education":0,"NAME_EDUCATION_TYPE_Secondary_or_secondary_special":0},{"CODE_GENDER_M":0,"NAME_CONTRACT_TYPE_Cash_loans":1,"NAME_EDUCATION_TYPE_Lower_secondary":1,"EXT_SOURCE_3":0.2485355573,"NAME_EDUCATION_TYPE_Higher_education":0,"NAME_EDUCATION_TYPE_Secondary_or_secondary_special":0}]
-    li_targets = [1, 1, 1]
-    for idx, (dict_features_i, out_i) in enumerate(zip(li_dict_features, li_targets)) :
+    # Send input data to prediction API via POST request 
+    req_post = requests.post(   url     = 'http://4.233.201.217:5677/invocations', 
+                                headers = {'Content-Type': 'application/json'}, 
+                                data    = '{"dataframe_split": {"index": [2582, 2583, 2584], "columns": ["NAME_EDUCATION_TYPE_Higher_education", "EXT_SOURCE_3", "EXT_SOURCE_2", "CODE_GENDER_M", "NAME_EDUCATION_TYPE_Secondary_or_secondary_special", "NAME_CONTRACT_TYPE_Cash_loans", "NAME_INCOME_TYPE_Working"], "data": [[0, 0.3944954053123993, 0.2731892846252296, 0, 1, 1, 1], [0, 0.1566398270314114, 0.5342365946929042, 1, 1, 1, 0], [0, 0.1176137317080569, 0.4562911010223129, 0, 1, 1, 0]]} }' )
+    
+    dict_predicted = json.loads(req_post.text)  # output predictions {'predictions': [1, 1, 1]}
+    df_result_grid['prediction'] = dict_predicted['predictions']
+    print(df_result_grid.round(3).T)            # results as pandas
 
-        dict_predicted_i = predict(dict_features_i)
-        dict_expected_i  = {'predictions':[out_i]}
-        print('input features   :', dict_features_i)
-        print('output predicted:', dict_predicted_i)
-        print('output expected :', dict_expected_i)
-
-        # Compare predicted & expected values
-        assert dict_predicted_i == dict_expected_i
+    # Compare predicted vs. expected values
+    assert df_result_grid['prediction'].equals(df_result_grid['target'])
 
