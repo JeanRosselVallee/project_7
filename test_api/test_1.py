280374eb972094a1afdfd47bd7564d29b56f5824
JeanRosselVallee <jv.isa4031@gmail.com>
2024-07-01 15:13:38 +0000

    Updated test catalog with new attributes' selection

--- test_api/test_1.py
+++ test_api/test_1.py
@@ -3,7 +3,7 @@ def test_connection():
     print('__________Connection to API Server_____________')
     req_post = requests.post(   url     = 'http://4.233.201.217:5677/invocations', 
                                 headers = {'Content-Type': 'application/json'}, 
-                                data    = '{"dataframe_split": {"index": [0], "columns": ["NAME_EDUCATION_TYPE_Higher_education", "EXT_SOURCE_3", "EXT_SOURCE_2", "CODE_GENDER_M", "NAME_EDUCATION_TYPE_Secondary_or_secondary_special", "NAME_CONTRACT_TYPE_Cash_loans", "NAME_INCOME_TYPE_Working"], "data": [[0, 0.5638350489514956, 0.3058183171273599, 0, 1, 1, 1]]} }' )
+                                data    = '{"dataframe_split": {"index": [0], "columns": ["CODE_GENDER_M", "NAME_CONTRACT_TYPE_Cash_loans", "EXT_SOURCE_3", "NAME_EDUCATION_TYPE_Higher_education", "EXT_SOURCE_2", "FLAG_OWN_CAR", "NAME_EDUCATION_TYPE_Secondary_or_secondary_special"], "data": [[1, 1, 0.1176137317080569, 0, 0.7088758643589249, true, 1]]} }' )
     # Evaluation of response
     is_connected = req_post.ok
     if is_connected : print('OK : Connected to API Server & validated input format')
--- test_api/test_2.py
+++ test_api/test_2.py
@@ -4,16 +4,21 @@ import pandas as pd
 import json
 
 def test_predict():
-    dict_inputs = {"index": [2582, 2583, 2584], "columns": ["NAME_EDUCATION_TYPE_Higher_education", "EXT_SOURCE_3", "EXT_SOURCE_2", "CODE_GENDER_M", "NAME_EDUCATION_TYPE_Secondary_or_secondary_special", "NAME_CONTRACT_TYPE_Cash_loans", "NAME_INCOME_TYPE_Working"], "data": [[0, 0.3944954053123993, 0.2731892846252296, 0, 1, 1, 1], [0, 0.1566398270314114, 0.5342365946929042, 1, 1, 1, 0], [0, 0.1176137317080569, 0.4562911010223129, 0, 1, 1, 0]]}                     # input observations      
+    dict_inputs = {"index": [2291, 2292, 2293], "columns": ["CODE_GENDER_M", "NAME_CONTRACT_TYPE_Cash_loans", "EXT_SOURCE_3", "NAME_EDUCATION_TYPE_Higher_education", "EXT_SOURCE_2", "FLAG_OWN_CAR", "NAME_EDUCATION_TYPE_Secondary_or_secondary_special"], "data": [[1, 1, 0.4740512892789932, 0, 0.3147092250216505, False, 1], [0, 1, 0.1595195404777181, 1, 0.3225519037906617, True, 0], [0, 1, 0.1261005588362325, 0, 0.3007587165928464, False, 1]]}                  # input observations      
     df_result_grid = pd.DataFrame(data=dict_inputs['data'], columns=dict_inputs['columns'])
     df_result_grid['target'] = [1, 1, 1]      # expected output
 
     # Send input data to prediction API via POST request 
     req_post = requests.post(   url     = 'http://4.233.201.217:5677/invocations', 
                                 headers = {'Content-Type': 'application/json'}, 
-                                data    = '{"dataframe_split": {"index": [2582, 2583, 2584], "columns": ["NAME_EDUCATION_TYPE_Higher_education", "EXT_SOURCE_3", "EXT_SOURCE_2", "CODE_GENDER_M", "NAME_EDUCATION_TYPE_Secondary_or_secondary_special", "NAME_CONTRACT_TYPE_Cash_loans", "NAME_INCOME_TYPE_Working"], "data": [[0, 0.3944954053123993, 0.2731892846252296, 0, 1, 1, 1], [0, 0.1566398270314114, 0.5342365946929042, 1, 1, 1, 0], [0, 0.1176137317080569, 0.4562911010223129, 0, 1, 1, 0]]} }' )
+                                data    = '{"dataframe_split": {"index": [2291, 2292, 2293], "columns": ["CODE_GENDER_M", "NAME_CONTRACT_TYPE_Cash_loans", "EXT_SOURCE_3", "NAME_EDUCATION_TYPE_Higher_education", "EXT_SOURCE_2", "FLAG_OWN_CAR", "NAME_EDUCATION_TYPE_Secondary_or_secondary_special"], "data": [[1, 1, 0.4740512892789932, 0, 0.3147092250216505, false, 1], [0, 1, 0.1595195404777181, 1, 0.3225519037906617, true, 0], [0, 1, 0.1261005588362325, 0, 0.3007587165928464, false, 1]]} }' )
     
     dict_predicted = json.loads(req_post.text)  # output predictions {'predictions': [1, 1, 1]}
+
+    
+    print(dict_predicted)
+
+    
     df_result_grid['prediction'] = dict_predicted['predictions']
     print(df_result_grid.round(3).T)            # results as pandas
 
